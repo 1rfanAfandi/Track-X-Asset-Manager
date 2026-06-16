f21f69ffaa3081f285a25fe2f40342151ad41c9c
1rfanAfandi <irfanafandi@student.telkomuniversity.ac.id>
2026-06-16 12:03:40 +0700

    Perubahan AssetListFrame

--- src/ui/karyawan/AssetListFrame.java
+++ src/ui/karyawan/AssetListFrame.java
@@ -4,25 +4,41 @@ import asset.dao.AssetDAO;
 import asset.model.Asset;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.util.List;
 
 public class AssetListFrame extends JFrame {
 
+    private JTable table;
+
     public AssetListFrame() {
+
+        initializeUI();
+
+        loadData();
+    }
+
+    private void initializeUI() {
 
         setTitle("Daftar Asset");
 
-        setSize(700,500);
+        setSize(900, 500);
 
         setLocationRelativeTo(null);
 
-        JTextArea area =
-                new JTextArea();
+        setDefaultCloseOperation(
+                JFrame.DISPOSE_ON_CLOSE);
 
-        JScrollPane scroll =
-                new JScrollPane(area);
+        table =
+                new JTable();
 
-        add(scroll);
+        add(
+                new JScrollPane(table),
+                BorderLayout.CENTER);
+    }
+
+    private void loadData() {
 
         AssetDAO dao =
                 new AssetDAO();
@@ -30,11 +46,33 @@ public class AssetListFrame extends JFrame {
         List<Asset> assets =
                 dao.selectAll();
 
+        DefaultTableModel model =
+                new DefaultTableModel();
+
+        model.addColumn("ID");
+        model.addColumn("Nama Asset");
+        model.addColumn("Status");
+        model.addColumn("Lokasi");
+        model.addColumn("Jenis");
+
         for (Asset asset : assets) {
 
-            area.append(
-                    asset.getInfo()
-                            + "\n\n");
+            model.addRow(
+                    new Object[] {
+
+                            asset.getIdAsset(),
+
+                            asset.getNama(),
+
+                            asset.getStatus(),
+
+                            asset.getLokasi(),
+
+                            asset.getClass()
+                                    .getSimpleName()
+                    });
         }
+
+        table.setModel(model);
     }
 }
